9598676520722f1179ef73d16f53670e5e72f973
annaindistress <annaindistress@gmail.com>
2020-07-15 00:14:34 +0300

    Добавляет модуль с генерацией данных

--- js/main.js
+++ js/main.js
@@ -1,17 +1,6 @@
 'use strict';
 
 // Константы
-
-var OFFER_AMOUNT = 8;
-var AVATAR_IMG_ADDRESS = 'img/avatars/user0';
-var AVATAR_IMG_FORMAT = '.png';
-var OFFER_TYPES = ['palace', 'flat', 'house', 'bungalo'];
-var OFFER_TIMES = ['12:00', '13:00', '14:00'];
-var OFFER_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var OFFER_PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-var PIN_X_MAX = 1200;
-var PIN_Y_MIN = 130;
-var PIN_Y_MAX = 630;
 var PIN_WIDTH = 50;
 var PIN_HEIGHT = 70;
 var IMAGE_WIDTH = 45;
@@ -31,42 +20,6 @@ var mapFilterForm = mapFilter.querySelector('.map__filters');
 
 var pinTemplate = document.querySelector('#pin').content;
 var cardTemplate = document.querySelector('#card').content;
-
-// Функция создания массива из сгенерированных JS-объектов
-
-var getOffersData = function (amount) {
-  var offers = [];
-
-  for (var i = 0; i < amount; i++) {
-    var offerLocationX = window.util.getRandomInt(PIN_X_MAX);
-    var offerLocationY = window.Uint8ClampedArray.util.getRandomInt(PIN_Y_MIN, PIN_Y_MAX);
-
-    offers[i] = {
-      author: {
-        avatar: AVATAR_IMG_ADDRESS + String(i + 1) + AVATAR_IMG_FORMAT
-      },
-      location: {
-        x: offerLocationX,
-        y: offerLocationY
-      },
-      offer: {
-        title: 'Заголовок предложения',
-        address: offerLocationX + ', ' + offerLocationY,
-        price: 0,
-        type: window.util.getRandomArrayElement(OFFER_TYPES),
-        rooms: 0,
-        guests: 0,
-        checkin: window.util.getRandomArrayElement(OFFER_TIMES),
-        checkout: window.util.getRandomArrayElement(OFFER_TIMES),
-        features: window.util.getRandomArray(OFFER_FEATURES),
-        description: 'Строка с описанием',
-        photos: window.util.getRandomArray(OFFER_PHOTOS)
-      }
-    };
-  }
-
-  return offers;
-};
 
 // Функция создания пин-метки на основе JS-объекта
 
@@ -248,7 +201,7 @@ var setActiveState = function () {
   window.form.timeOut.addEventListener('change', window.form.onChangeTime);
   window.form.type.addEventListener('change', window.form.onChangeType);
 
-  renderPinList(offers);
+  renderPinList(window.data.offers);
 };
 
 // Функция, открывающая карточку
@@ -265,7 +218,7 @@ var openCard = function (element) {
     closeCard();
   }
 
-  mapFilter.insertAdjacentElement('beforeBegin', renderCardItem(offers[index - 1]));
+  mapFilter.insertAdjacentElement('beforeBegin', renderCardItem(window.data.offers[index - 1]));
 
   document.addEventListener('keydown', onCardEscPress);
 
@@ -295,7 +248,6 @@ var onCardEscPress = function (evt) {
 // Основная часть
 
 setInactiveState();
-var offers = getOffersData(OFFER_AMOUNT);
 
 // Запуск страницы при взаимодействии с pinMain
 
