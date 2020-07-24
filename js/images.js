90866b1dfb2d6f7c4b86bd2bcfcae4fe2235c9df
Anna <annaindistress@gmail.com>
2020-07-24 03:47:59 +0300

    Добавляет загрузку аватара и фотографий

---
+++ js/images.js
@@ -0,0 +1,62 @@
+'use strict';
+
+(function () {
+  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+
+  var avatarChooser = document.querySelector('#avatar');
+  var avatarPreview = document.querySelector('.ad-form-header__preview img');
+  var properyPhotoChooser = document.querySelector('#images');
+  var properyPhotoPreview = document.querySelector('.ad-form__photo');
+
+  var onAvatarChange = function () {
+    var file = avatarChooser.files[0];
+    var fileName = file.name.toLowerCase();
+
+    var matches = FILE_TYPES.some(function (it) {
+      return fileName.endsWith(it);
+    });
+
+    if (matches) {
+      var reader = new FileReader();
+
+      reader.addEventListener('load', function () {
+        avatarPreview.src = reader.result;
+      });
+
+      reader.readAsDataURL(file);
+    }
+  };
+
+  var onProperyPhotoChange = function () {
+    Array.from(properyPhotoChooser.files).forEach(function (file) {
+      var fileName = file.name.toLowerCase();
+
+      var matches = FILE_TYPES.some(function (it) {
+        return fileName.endsWith(it);
+      });
+
+      if (matches) {
+        var reader = new FileReader();
+
+        reader.addEventListener('load', function () {
+          var photo = document.createElement('img');
+          photo.width = 70;
+          photo.height = 70;
+          photo.style.margin = '0 10px 10px 0';
+          photo.scr = 'Фотографии предложения';
+          photo.src = reader.result;
+          properyPhotoPreview.appendChild(photo);
+        });
+
+        reader.readAsDataURL(file);
+      }
+    });
+  };
+
+  window.images = {
+    avatar: avatarChooser,
+    onAvatarChange: onAvatarChange,
+    property: properyPhotoChooser,
+    onProperyPhotoChange: onProperyPhotoChange
+  };
+})();
